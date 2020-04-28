139534fd341019715f3704c8282c61d79133200e
Kellian Cottart <kellian.cottart@edu.ece.fr>
2020-04-28 12:28:10 +0200

    Petit menu

--- main.cpp
+++ main.cpp
@@ -9,14 +9,15 @@ int main()
 
     Graphe* G=new Graphe();
     int choix=0;
-    std::cout<<"_________MENU_________\n\n";
-    std::cout << "1. Charger le graphe\n";
-    std::cout << "2. Afficher le graphe dans le texte\n";
-    std::cout << "3. Dessiner le graphe en SVG\n";
-    std::cout << "4. Calculer et exporter les indices\n";
-    std::cout << "\n\n9. Quitter\n";
+
     while (choix!=9)
     {
+         std::cout<<"_________MENU_________\n\n";
+        std::cout << "1. Charger le graphe\n";
+        std::cout << "2. Dessiner le graphe en SVG\n";
+        std::cout << "3. Afficher le graphe dans le texte\n";
+        std::cout << "4. Calculer et exporter les indices\n";
+        std::cout << "\n\n9. Quitter\n";
         do
         {
         std::cin>>choix;
@@ -25,24 +26,33 @@ int main()
         switch(choix)
         {
             case 1:
-                G->chargerGraphe("graphe_cycle4_topo.txt", "graphe_cycle4_poids.txt" );
+            {
+                std::string topo, ponderation;
+                std::cout << "Entrez le nom du fichier topologique puis du fichier de ponderation s'il y en a un"<<std::endl;
+                std::cin >> topo;
+                fflush(stdin);
+                getline(std::cin, ponderation);
+                G->chargerGraphe(topo, ponderation);
+                std::cout<< "Le graphe est charge\n\n";
                 break;
+            }
+
             case 2:
-                G->affichageTextuel();
-                break;
-            case 3:
                 {
                     Svgfile svgout;
                     G->dessinGraphe(svgout);
-                    std::cout<< "Le dessin est effectue\n";
+                    std::cout<< "Le dessin est effectue\n\n";
                     break;
                 }
+            case 3:
+                G->affichageTextuel();
+                break;
             case 4:
                 for (auto s : G->getVectS())
                     s->getVectI()[0]->calculIndice();
                 G->getVectS()[0]->getVectI()[1]->calculIndice();
                 G->sauvegardeIndice();
-                std::cout<< "Le calcul est effectue et exporte\n";
+                std::cout<< "Le calcul est effectue et exporte\n\n";
                 break;
         }
     }
